29a0ca84ed1265fc2a0236c5bd785f0e275378f3
sushma <sushma@TeamCity>
2021-02-18 10:10:33 +0530

    TeamCity change in 'Javascript' project: triggers of '01. Fast test' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
@@ -49,6 +50,13 @@ object id01FastTest : BuildType({
 
     params {
         param("Browser", "PhantomJS")
+    }
+
+    triggers {
+        vcs {
+            id = "vcsTrigger"
+            branchFilter = ""
+        }
     }
 })
 
